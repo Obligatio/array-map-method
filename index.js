be1d41b124079331023dbe8f7b751c91a064b715
kevin <kevinke386@gmail.com>
2024-03-31 18:37:59 +0300

    map method

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const tutorials = [
+    "What Does The This Keyword Mean?",
+    'What Is The Constructor OO Pattern?',
+    'Implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    "What Is NaN And How Can We Check For It",
+    'What Is The Difference Between StopPropagation And PreventDefault?',
+    'Immutable State And Pure Functions',
+    "What Is The Difference Between == And ===?",
+    "What Is The Difference Between Event Capturing And Bubbling?",
+    "What Is JSONP?"
+  ];
+  
+  const titleCased = () =>{
+    return tutorials.map(first=>first.charAt(0).toUpperCase()+first.slice(1))
+    };
+  
+  
